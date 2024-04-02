dbcbc0fb7f02af7f251c3a5f02bc7017a0b12871
houggadi <hamza.ouggadi@capgemini.com>
2024-04-02 12:39:55 +0000

    Added feature routing

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: Product | null = null;
   errorMessage = '';
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private activatedRoute : ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id')!;
+    if (id !== null && !isNaN(id)) {
+      this.getProduct(id);
+    } else {
+      console.error("id is null");
+    }
+  }
 
   getProduct(id: number): void {
     this.productService.getProduct(id).subscribe({
